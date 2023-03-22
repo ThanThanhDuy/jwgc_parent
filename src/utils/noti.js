2c55a208c13ed8ecbadeaff940d5bf9fc1b7cfd0
Thân Thanh Duy <84673591+ThanThanhDuy@users.noreply.github.com>
2023-03-22 22:33:37 +0700

    [DuyTT] fix noti

--- src/utils/noti.js
+++ src/utils/noti.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/messaging";
+import firebase from "firebase/compat/app";
+import "firebase/compat/messaging";
 import { firebaseConfig } from "./firebase";
 
 if (!firebase.apps.length) {
